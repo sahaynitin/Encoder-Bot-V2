0facd9edc34f873a6dfb7afe61c4384842f60f39
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-10 13:39:45 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -21,10 +21,10 @@ async def start(event):
         f"**Hey {event.sender.first_name}\n\nThis is A CompressorBot Which Can Encode Videos.\n\nReduce Size of Videos With Negligible Quality Change\n**",
         buttons=[
             
-            [Button.url("Update Channel", url="github.com/1Danish-00/CompressorBot"),
-                Button.url("Support Group", url="t.me/danish_00"),
+            [Button.inline("HELP", data="help"),
+                Button.url("ABOUT", data="about"),
             ],
-        [Button.inline("HELP", data="help")],
+        [Button.inline("Close", data="close")],
   ],
     )
 
@@ -52,6 +52,10 @@ async def help(event):
         f"""**To check current ffmpeg code you can use** /getcode\n\n**You can change your ffmpeg code by executing following command.**\n\n`/setcode -preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1`\n\n**To set custom thumbnail send me the image.**\n\n**Do /cmds For More**"""
     )
 async def close(event):
+    await event.edit(
+        f"""****"""
+    )
+async def about(event):
     await event.edit(
         f"""****"""
     )
